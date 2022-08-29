5b2e36abfd8801e2d03628d97313f25981f38555
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-08-29 12:57:35 +0300

    Update serving.py

--- serving.py
+++ serving.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import mlrun
 from mlrun import MLClientCtx
-def data_fetch(num_rows):
-    values=[]
-    context=MLClientCtx()
-    for i in num_rows:
-        values.append(i)
-    df = pd.DataFrame(data={"col1":values})
-    context.log_dataset(key='df_1',df=df,format='csv')
+def data_fetch(num):
+    return num
+
         
