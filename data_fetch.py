3023ab219e7ff29b6c60cff45ac692beaec4e902
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-08-30 14:54:58 +0300

    Add sleep

--- data_fetch.py
+++ data_fetch.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import mlrun
 from mlrun import MLClientCtx
+import time
 def data_fetch(context:MLClientCtx):
+    time.sleep(300)
     df = pd.DataFrame(data={"col1":[1,2,3,4,5,6]})
     context.log_dataset(key='df_2',df=df,format='csv')
         
