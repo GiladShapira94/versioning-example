bbf4421273be6432ec5acaaf163e00cee19201a5
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-08-30 14:51:25 +0300

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -2,6 +2,6 @@ import pandas as pd
 import mlrun
 from mlrun import MLClientCtx
 def data_fetch(context:MLClientCtx):
-    df = pd.DataFrame(data={"col1":[1,2,3,4,5]})
+    df = pd.DataFrame(data={"col1":[1,2,3,4,5,6]})
     context.log_dataset(key='df_2',df=df,format='csv')
         
