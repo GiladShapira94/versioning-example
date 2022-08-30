b3a65ce5c75543c9fcfd16e61b45fa12ffa0b10f
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-08-30 15:09:21 +0300

    change to df 3

--- data_fetch.py
+++ data_fetch.py
@@ -5,5 +5,5 @@ import time
 def data_fetch(context:MLClientCtx):
     time.sleep(300)
     df = pd.DataFrame(data={"col1":[1,2,3,4,5,6]})
-    context.log_dataset(key='df_2',df=df,format='csv')
+    context.log_dataset(key='df_3',df=df,format='csv')
         
